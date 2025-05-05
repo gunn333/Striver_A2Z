ada2d7acf5769a97093875b8d57caa6e69d4a6f4
Gunn Arora <122155330+gunn333@users.noreply.github.com>
2025-05-05 10:12:32 +0530

    SubArray Product Q2

--- Arrays/Easy/SubArray_Prod_LessThanK.java
+++ Arrays/Easy/SubArray_Prod_LessThanK.java
@@ -23,3 +23,28 @@ class Solution {
         return count;
     }
 }
+
+// Alternate Approach:
+
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k<= 1){
+            return 0;
+        }
+
+        int product = 1;
+        int count = 0;
+        int left = 0;
+
+        for(int right = 0; right < nums.length; right++){
+            product *= nums[right];
+
+            while(product >= k){
+                product /= nums[left];
+                left++;
+            }
+            count += right-left+1;
+        }
+        return count;
+    }
+}
