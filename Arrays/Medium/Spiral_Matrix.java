0555aba7104f0994df551404251cbdbd6f42a744
Gunn Arora <122155330+gunn333@users.noreply.github.com>
2025-05-07 12:22:33 +0530

    Spiral Matrix

---
+++ Arrays/Medium/Spiral_Matrix.java
@@ -0,0 +1,39 @@
+// Link : https://leetcode.com/problems/spiral-matrix/
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList<Integer> spiral = new ArrayList<>();
+        int top = 0;
+        int left = 0;
+        int right = matrix[0].length-1;
+        int bottom = matrix.length-1;
+
+        while(left<=right && top <= bottom){
+             
+             for(int i = left; i<= right; i++){
+                spiral.add(matrix[top][i]);
+             }
+             top++;
+
+             for(int i = top; i<= bottom; i++){
+                spiral.add(matrix[i][right]);
+             }
+             right--;
+
+             if(top<=bottom){
+                for(int i = right; i >= left; i--){
+                    spiral.add(matrix[bottom][i]);
+                }
+                bottom--;
+             }
+
+             if(left <= right){
+                for(int i=bottom; i>=top; i--) {
+					spiral.add(matrix[i][left]);
+				}
+				left++;
+             }
+        }
+        return spiral;
+    }
+}
